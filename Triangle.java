efe3f8bf87d5d1fbee8cd134e16562fca01bc697
HemaShaswar <96527414+HemaShaswar@users.noreply.github.com>
2024-03-24 19:40:23 +0300

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -1,25 +1,29 @@
 public class Triangle extends Shape{
 
-    public Triangle(String color, Point center, String type) 
+    private Point cornerOne, cornerTwo, cornerThree;
+    private double area;
+    public Triangle(Point cornerOne, Point cornerTwo, Point cornerThree, String color, String type) {
+        super(color, type);
+        this.cornerOne = cornerOne;
+        this.cornerTwo = cornerTwo;
+        this.cornerThree = cornerThree;
+        setArea();
+    }
+    
+    @Override
+    public double getArea()
     {
-        super(color);
-        System.out.println("Please provide the coordinates of the shape");
+        return area;
+    }
+
+    public void setArea() {
         
+        area = 0.5 * Math.abs((cornerOne.x * (cornerTwo.y - cornerThree.y) + cornerTwo.x * (cornerThree.y - cornerOne.y) + cornerThree.x * (cornerOne.y - cornerTwo.y)));
     }
 
     @Override
     public String toString()
     {
-        return super.toString() + "\nShape Position: " + getPosition();
+        return super.toString() + "\nTriangle area: " + getArea();
     }
-
-    public void setPosition(double x, double y, double z)
-    {
-        
-    }
-    public String getPosition()
-    {
-        return "a";
-    }
-
 }
