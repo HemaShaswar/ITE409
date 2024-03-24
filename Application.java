f94cb35d17fafc7558edac9a246533da20dc18ce
HemaShaswar <96527414+HemaShaswar@users.noreply.github.com>
2024-03-25 00:28:56 +0300

    Update Application.java

--- Application.java
+++ Application.java
@@ -1,52 +1,108 @@
+import java.util.Scanner;
 
 public class Application 
 {
-    public static void main(String... args) 
-    {
-        System.out.println("Please provide the coordinates of the shape, the color, and the type: ");
-
+    public static void main(String... args) {
         
+        Shape[] newShapes = new Shape[10];
+        for(int i = 0;i<10;i++){
+            String shapeType = askUserShapeType();
+            String color = askUserColor();
 
-        System.out.println();
+    
+            switch (shapeType) {
+                case "square":
+                    createSquare(color);
+                    break;
+                case "cube":
+                    createCube(color);
+                    break;
+                case "triangle":
+                    createTriangle(color);
+                    break;
+                case "circle":
+                    createCircle(color);
+                    break;
+                case "sphere":
+                    createSphere(color);
+                    break;
+                case "exit":
+                    break;
+                default:
+                    System.out.println("Invalid Shape Entered, try again: ");
+                    askUserShapeType();
+                    break;
+            }
 
-        Point a = new Point(100, 3);
-        Point b = new Point(4, 37);
-        Point c = new Point(70, 5);
-
-        Triangle triangle = new Triangle(a,b,c,"red","triangle");
+        }
         
-        System.out.println(triangle.toString());
-
-
-        Point d = new Point(100, 3);
-        Point e = new Point(4, 37);
-       
-
-        Cube cube = new Cube(a,b,"red","cube");
-        
-        System.out.println(cube.toString());
-
-
-
-
-        System.out.println();
-
-        Point f = new Point(10, 4);
-        Point g = new Point(37, 70);
-        Square square = new Square(f,g,"green","square");
-        System.out.println(square.toString());
-
-        System.out.println();
-
-        Point h = new Point(10, 4, 80);
-        Point i = new Point(37, 70, 90);
-
-        Circle c1 = new Circle("red", h, i);
-        System.out.println(c1.toString());
-
-        System.out.println();
-
-        Sphere s1 = new Sphere("white", h, i);
-        System.out.println(s1.toString());
     }
+
+    public static String askUserShapeType(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter Shape Type to Create: ");
+        System.out.println("(Square, Cube, Circle, Sphere, Triangle, Or 'Exit')");
+        String shapeType = input.nextLine().toLowerCase();
+        return shapeType;
+    }
+
+    public static Circle createCircle(String color){
+        Point centerPoint = askUserPoint();
+        Point circumPoint = askUserPoint();
+
+        return new Circle(color, centerPoint, circumPoint);
+    }
+
+    public static Sphere createSphere(String color){
+        Point centerPoint = askUserPoint();
+        Point circumPoint = askUserPoint();
+
+        return new Sphere(color, centerPoint, circumPoint);
+    }
+
+    public static Square createSquare(String color){
+        Point corner1 = askUserPoint();
+        Point corner2 = askUserPoint();
+
+        return new Square(corner1, corner2, color);
+    }
+
+    public static Cube createCube(String color){
+        Point corner1 = askUserPoint();
+        Point corner2 = askUserPoint();
+
+        return new Cube(corner1, corner2, color);
+    }
+
+    public static Triangle createTriangle(String color){
+        Point corner1 = askUserPoint();
+        Point corner2 = askUserPoint();
+        Point corner3 = askUserPoint();
+
+        return new Triangle(corner1, corner2, corner3, color);
+    }
+
+    public static Point askUserPoint(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter Point Intiger Coordinates: ");
+        System.out.println("Point.X Intiger Value: ");
+        int x1 = input.nextInt();
+        System.out.println("Point.Y Intiger Value: ");
+        int y1 = input.nextInt();
+        System.out.println("Point.Z Intiger Value: ");
+        int z1 = input.nextInt();
+
+        return new Point(x1, y1, z1);
+    }
+
+    public static String askUserColor(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Shape Color: ");
+        String color = input.nextLine().toLowerCase();
+        return color;
+
+    }
+    
 }
