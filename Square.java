174c745d45e20f6933edb4380292ab39d792b3aa
HemaShaswar <96527414+HemaShaswar@users.noreply.github.com>
2024-03-24 19:40:39 +0300

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,26 +1,40 @@
 public class Square extends Shape
 {
 
-    public Square(String color) 
+    private double area;
+    private Point cornerOne, cornerTwo;
+
+    public Square(Point cornerOne, Point cornerTwo, String color, String type) 
     {
-        super(color);
-        System.out.println("Please provide the coordinates of the shape");
-        
+        super(color, type);
+        this.cornerOne = cornerOne;
+        this.cornerTwo = cornerTwo;
+        setSize();
     }
 
+   
+    public double getSize()
+    {
+        return area;
+    }
+
+    public void setSize() {
+        double sideLength = Math.sqrt(Math.pow(cornerTwo.x - cornerOne.x, 2) + Math.pow(cornerTwo.y - cornerOne.y, 2));
+        area = sideLength * sideLength;
+    }
+
+    
     @Override
     public String toString()
     {
-        return super.toString() + "\nShape Position: " + getPosition();
+        return super.toString() + "\nSquare area: " + getSize();
     }
 
-    public void setPosition(double x, double y, double z)
-    {
-        
-    }
-    public String getPosition()
-    {
-        return "a";
+
+    @Override
+    public double getArea() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getArea'");
     }
 
 }
